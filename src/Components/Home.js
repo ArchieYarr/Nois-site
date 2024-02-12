b32572a67a6dbc2af6c2b990f6c29b2458ac0347
ArchieYarr <AYARR200@gcu.ac.uk>
2024-02-12 16:50:19 +0000

    initial react app created with tailwind installed.

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home(){
+
+
+    return(
+        <div> HELOOOO</div>
+
+    );
+}
+
+export default Home;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React,  { useState, useEffect, useRef } from "react";
+import { HashLink } from "react-router-hash-link";
+
+function Navbar(){
+
+
+    return(
+        <div> I be the navbar meharty</div>
+
+    );
+}
+
+export default Navbar;
